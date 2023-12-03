8c545b1546b43e4081f9f6374266a300b7a1ee40
Dario Lencina <dario@securityunion.dev>
2023-12-03 13:04:12 -0500

    save

--- day-3/target/debug/day-3.d
+++
@@ -1,1 +0,0 @@
-/Users/darioalessandro/Documents/advent-of-code-2023/day-3/target/debug/day-3: /Users/darioalessandro/Documents/advent-of-code-2023/day-3/src/main.rs
--- day-3/target/debug/deps/day_3-d162555005f2c510.d
+++
@@ -1,5 +0,0 @@
-/Users/darioalessandro/Documents/advent-of-code-2023/day-3/target/debug/deps/day_3-d162555005f2c510.rmeta: src/main.rs
-
-/Users/darioalessandro/Documents/advent-of-code-2023/day-3/target/debug/deps/day_3-d162555005f2c510.d: src/main.rs
-
-src/main.rs:
